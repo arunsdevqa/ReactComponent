ee67c6f07a0329328d1a4f3c463e2a338e6372ab
arunsdevqa <sarunmca2007@gmail.com>
2017-06-14 13:23:46 -0700

    Add files via upload

---
+++ HelloPlanet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class HelloPlanet extends React.Component{
+    render(){
+        return <div>
+            <span>Type sample text:</span> <input type="text" id="txtTest"/>
+            <h1>Hello, I am from HelloPlanet components!</h1>
+                Hello Earth! Welcome to my 2nd React application
+        </div>
+    }
+}
---
+++ MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class MyComponent extends React.Component{
+    render(){
+        return <div>
+            <h1>Hello, I am from MyComponent components!</h1>
+                Hello World! Welcome to my 2nd React application
+        </div>
+    }
+}
